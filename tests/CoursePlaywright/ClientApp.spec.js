293fcef66492087589ea7c41e1c9e0edf195086b
RutujaWayase <rutujawayase1234@gmail.com>
2026-07-18 07:53:55 +0530

    adding assertions on actions

--- tests/CoursePlaywright/ClientApp.spec.js
+++ tests/CoursePlaywright/ClientApp.spec.js
@@ -1,16 +1,34 @@
 const {test, expect} = require('@playwright/test');
 
 test.only('Browser contxt-validation error login', async ({page}) => {
+
+    const productName = 'ZARA COAT 3';
+    const products = page.locator(".card-body");
     await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
     await page.locator("#userEmail").fill("anshika@gmail.com");
     await page.locator("#userPassword").fill("Iamking@000");
     //await page.locator("#login").click();
     await page.locator("[value='Login']").click();
    // Approach 1: Wait for Network tab =>
-   // await page.waitForLoadState('networkidle');
+   await page.waitForLoadState('networkidle');
    //Approach 2: Directly applying wait to an element
    await page.locator(".card-body b").first().waitFor();
     const titles = await page.locator(".card-body b").allTextContents();
     console.log(titles);
+    const count = await products.count();
+    for(let i=0; i<count; ++i){
+        if(await products.nth(i).locator("b").textContent() === productName)
+        {
+            //add to cart
+            await products.nth(i).locator("text= Add To Cart").click();
+            break;
+        }
+    }
+    //await page.pause();
+    await page.locator("[routerlink*='cart']").click();
+    await page.locator("div li").first().waitFor();
+    const bool = await page.locator("h3:has-text('Zara Coat 3')").isVisible();
+    expect(bool).toBeTruthy();
+
 
 })
