91bcaaa4d1b7c58c3dc994a6fc6b8872c3fdc119
RutujaWayase <rutujawayase1234@gmail.com>
2026-07-22 12:30:32 +0530

    adding how to hanle and automate frames

--- tests/CoursePlaywright/MoreValidations.spec.js
+++ tests/CoursePlaywright/MoreValidations.spec.js
@@ -9,9 +9,13 @@ test("Popup validations", async({page}) => {
     await expect(page.locator("#displayed-text")).toBeVisible();
     await page.locator("#hide-textbox").click();
     await expect(page.locator("#displayed-text")).toBeHidden();
-    await page.pause();
+    //await page.pause();
     page.on('dialog', dialog => dialog.accept()); // accept
-    page.on('dialog', dialog => dialog.dismiss()); //dismiss
+    //page.on('dialog', dialog => dialog.dismiss()); //dismiss
     await page.locator("#confirmbtn").click();
     await page.locator("#mousehover").hover();
+    const framesPage = page.frameLocator("#courses-iframe");
+    await framesPage.locator("li a[href*='lifetime-access']:visible").click();
+    const textCheck = await framesPage.locator(".text h2").textContent();
+    console.log(textCheck.split(" ")[1]);
 })
