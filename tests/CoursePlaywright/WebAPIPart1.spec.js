91bd99c6369336068d669c15cd250bebbcd1626f
RutujaWayase <rutujawayase1234@gmail.com>
2026-07-25 07:58:14 +0530

    adding api calls from utils and isolate from web test logic

--- tests/CoursePlaywright/WebAPIPart1.spec.js
+++ tests/CoursePlaywright/WebAPIPart1.spec.js
@@ -1,91 +1,155 @@
 const {test, expect, request} = require('@playwright/test');
-const loginPayLoad = {userEmail: "anshika@gmail.com", userPassword: "Iamking@000"};
-let token;
-
-test.beforeAll(async() => {
-    const apiContext = await request.newContext();
-    const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
-        {
-            data: loginPayLoad
-        }
-    ) //200, 201,
-    expect(loginResponse.ok()).toBeTruthy();
-    const loginResponseJson = loginResponse.json();
-    token = loginResponseJson.token;
-    console.log(token);
-
-});
-
-test.beforeEach( () => 
+const {APiUtils} = require('./utils/APiUtils');
+const loginPayLoad = {userEmail:"anshika@gmail.com",userPassword:"Iamking@000"};
+const orderPayLoad = {orders:[{country:"Cuba",productOrderedId:"67a8dde5c0d3e6622a297cc8"}]};
+ 
+ 
+let response;
+test.beforeAll( async()=>
 {
-
+   const apiContext = await request.newContext();
+   const apiUtils = new APiUtils(apiContext,loginPayLoad);
+   response =  await apiUtils.createOrder(orderPayLoad);
+ 
 })
-
-//test 1, test2, test3
-
-test('Place the order', async ({page}) => {
-
+ 
+ 
+//create order is success
+test('@API Place the order', async ({page})=>
+{ 
     await page.addInitScript(value => {
-        window.localStorage.setItem('token', value);
-    }, token);
+ 
+        window.localStorage.setItem('token',value);
+    }, response.token );
+await page.goto("https://rahulshettyacademy.com/client");
+ await page.locator("button[routerlink*='myorders']").click();
+ await page.locator("tbody").waitFor();
+const rows = await page.locator("tbody tr");
+ 
+ 
+for(let i =0; i<await rows.count(); ++i)
+{
+   const rowOrderId =await rows.nth(i).locator("th").textContent();
+   if (response.orderId.includes(rowOrderId))
+   {
+       await rows.nth(i).locator("button").first().click();
+       break;
+   }
+}
+const orderIdDetails =await page.locator(".col-text").textContent();
+//await page.pause();
+expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
+ 
+});
+ 
+//Verify if order created is showing in history page
+// Precondition - create order -
 
-    // const productName = 'ZARA COAT 3';
-    // const products = page.locator(".card-body");
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const {test, expect, request} = require('@playwright/test');
+// const loginPayLoad = {userEmail: "anshika@gmail.com", userPassword: "Iamking@000"};
+// let token;
+
+// test.beforeAll(async() => {
+//     const apiContext = await request.newContext();
+//     const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
+//         {
+//             data: loginPayLoad
+//         }
+//     ) //200, 201,
+//     expect(loginResponse.ok()).toBeTruthy();
+//     const loginResponseJson = loginResponse.json();
+//     token = loginResponseJson.token;
+//     console.log(token);
+
+// });
+
+// test.beforeEach( () => 
+// {
+
+// })
+
+// //test 1, test2, test3
+
+// test('Place the order', async ({page}) => {
+
+//     await page.addInitScript(value => {
+//         window.localStorage.setItem('token', value);
+//     }, token);
+
+//     // const productName = 'ZARA COAT 3';
+//     // const products = page.locator(".card-body");
+
+
+// //     await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+// //     await page.locator("#userEmail").fill("anshika@gmail.com");
+// //     await page.locator("#userPassword").fill("Iamking@000");
+// //     //await page.locator("#login").click();
+// //     await page.locator("[value='Login']").click();
+// //    // Approach 1: Wait for Network tab =>
+// //    await page.waitForLoadState('networkidle');
+//    //Approach 2: Directly applying wait to an element
+// //   await page.locator(".card-body b").first().waitFor();
+
+//     const email = "";
+//     const productName = 'ZARA COAT 3';
 //     await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
-//     await page.locator("#userEmail").fill("anshika@gmail.com");
-//     await page.locator("#userPassword").fill("Iamking@000");
-//     //await page.locator("#login").click();
-//     await page.locator("[value='Login']").click();
-//    // Approach 1: Wait for Network tab =>
-//    await page.waitForLoadState('networkidle');
-   //Approach 2: Directly applying wait to an element
-//   await page.locator(".card-body b").first().waitFor();
+//     const products = page.locator(".card-body");
+//     const titles = await page.locator(".card-body b").allTextContents();
+//     console.log(titles);
+//     const count = await products.count();
+//     for(let i=0; i<count; ++i){
+//         if(await products.nth(i).locator("b").textContent() === productName)
+//         {
+//             //add to cart
+//             await products.nth(i).locator("text= Add To Cart").click();
+//             break;
+//         }
+//     }
+//     //await page.pause();
+//     await page.locator("[routerlink*='cart']").click();
+//     await page.locator("div li").first().waitFor();
+//     const bool = await page.locator("h3:has-text('Zara Coat 3')").isVisible();
+//     expect(bool).toBeTruthy();
+//     await page.locator("text=Checkout").click();
 
-    const email = "";
-    const productName = 'ZARA COAT 3';
-    await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
-    const products = page.locator(".card-body");
-    const titles = await page.locator(".card-body b").allTextContents();
-    console.log(titles);
-    const count = await products.count();
-    for(let i=0; i<count; ++i){
-        if(await products.nth(i).locator("b").textContent() === productName)
-        {
-            //add to cart
-            await products.nth(i).locator("text= Add To Cart").click();
-            break;
-        }
-    }
-    //await page.pause();
-    await page.locator("[routerlink*='cart']").click();
-    await page.locator("div li").first().waitFor();
-    const bool = await page.locator("h3:has-text('Zara Coat 3')").isVisible();
-    expect(bool).toBeTruthy();
-    await page.locator("text=Checkout").click();
+//     //await page.locator("[placeholder*='Country']").fill("ind", {delay:100});
 
-    //await page.locator("[placeholder*='Country']").fill("ind", {delay:100});
+//     //use pressSequentially() instead of fill()/type()
 
-    //use pressSequentially() instead of fill()/type()
+//     await page.locator("[placeholder*='Country']").pressSequentially("ind", {delay:100});
 
-    await page.locator("[placeholder*='Country']").pressSequentially("ind", {delay:100});
+//     const dropdown = page.locator(".ta-results");
+//     await dropdown.waitFor();
+//     const optionsCount = await dropdown.locator("button").count();
+//     for(let i=0; i < optionsCount; ++i)
+//     {
+//         const text = await dropdown.locator("button").nth(i).textContent();
+//         if(text === " India")
+//             //if(text.trim === "India")
+//             //if(text.includes("India")) ==> but other options also includes "India"
+//         {
+//             await dropdown.locator("button").nth(i).click();
+//             break;
+//         }
+//     }
+//     //await page.pause();
 
-    const dropdown = page.locator(".ta-results");
-    await dropdown.waitFor();
-    const optionsCount = await dropdown.locator("button").count();
-    for(let i=0; i < optionsCount; ++i)
-    {
-        const text = await dropdown.locator("button").nth(i).textContent();
-        if(text === " India")
-            //if(text.trim === "India")
-            //if(text.includes("India")) ==> but other options also includes "India"
-        {
-            await dropdown.locator("button").nth(i).click();
-            break;
-        }
-    }
-    //await page.pause();
-
-})
+// })
 
